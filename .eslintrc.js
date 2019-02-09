4a9af570b8e3855db88136c32cad72f2a844685c
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-09 06:23:45 +0100

    Project init setup

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": ["error", 2],
+        "linebreak-style": ["error", "windows"],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+        "comma-dangle": ["error", "never"],
+        "eol-last": ["error", "always"],
+        "no-multiple-empty-lines": [
+            "error",
+            { "max": 1, "maxBOF": 0, "maxEOF": 0 }
+        ],
+        "no-console": "warn",
+        "no-unused-vars": "warn",
+    }
+};
